317ddeae9729e3d3a1c0cb7199a6e67ac063f646
Pavel Kravchenko <pkravchenko@biochem.mpg.de>
2024-03-22 15:49:51 +0100

    22032024 release

--- figures/Figure_S3/Figure_S3_join_ATAC.sh
+++
@@ -1,4 +0,0 @@
-#split -b 40M ./ATAC_seq_2cell_DMSO_merged_rep1_rep2_from_Gassler_et_al_Science_2022.bedgraph ./ATAC_segments/ATAC_segment
-
-# Please assemble the complete file before starting the analysis
-cat ./ATAC_segments/ATAC_segment* > ./ATAC_seq_2cell_DMSO_merged_rep1_rep2_from_Gassler_et_al_Science_2022.bedgraph
